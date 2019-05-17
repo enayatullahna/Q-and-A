5cd6a7cc74551f15f0dd7875031f0bc89c2b972a
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-16 19:03:43 -0700

    Created table view scene and table view conteroller file and cell controller file

---
+++ Q-and-A/Controller/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  Q-and-A
+//
+//  Created by Enayatullah Naseri on 5/16/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
